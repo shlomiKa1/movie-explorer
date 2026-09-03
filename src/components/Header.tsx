d40a30601da9c09503281cf51411f3b3fef55b12
Shlomi <shlomikaufman91@gmail.com>
2026-09-03 14:06:46 +0300

    feat: add navbar

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { Link, Outlet } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="navbar">
+        <p>🍿🎬 Movie Explorer</p>
+        <Link to="/movies">Movies</Link>
+        <Link to="/movies/favorites">Favorites</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
