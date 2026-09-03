2fe540c21da1d7f51ea7cb408ce6493048b779c8
Shlomi <shlomikaufman91@gmail.com>
2026-09-03 14:05:04 +0300

    Feat: add page for movie details

---
+++ src/pages/MovieDetails.tsx
@@ -0,0 +1,37 @@
+import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+import { useMoviesStore } from "../store/MoviesStore";
+import {
+  BASE_URL,
+  mapTvmazeShowToInternal,
+  type TvmazeShowRaw,
+} from "../utils/types";
+import MovieDetailsPage from "../components/MovieDetailsPage";
+
+const MovieDetails = () => {
+  const { id } = useParams();
+  console.log("id from params:", id);
+
+  const movieFromStore = useMoviesStore((state) =>
+    state.getMovieById(Number(id)),
+  );
+
+  const URL = BASE_URL.concat(`/${id}`);
+  const { data, loading, error } = useFetch<TvmazeShowRaw>(
+    URL,
+    !movieFromStore,
+  );
+
+  const movie = movieFromStore ?? (data ? mapTvmazeShowToInternal(data) : null);
+  if (!movieFromStore && loading) return <p>Loading data...</p>;
+  if (!movieFromStore && error) return <p>404 {error}</p>;
+  if (!movie) return <p>סרט לא נמצא</p>;
+
+  return (
+    <section>
+      <MovieDetailsPage movie={movie} />
+    </section>
+  );
+};
+
+export default MovieDetails;
