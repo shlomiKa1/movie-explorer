085c5882d9bf3f375da14ef9218e43dcab6a24bb
Shlomi <shlomikaufman91@gmail.com>
2026-09-04 09:22:41 +0300

    feat: add counts of favorite movie at navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
+import { useFavoritesStore } from "../store/FavoritesStore";
 
 const Header = () => {
+  const favorites = useFavoritesStore((state) => state.favorites);
+
   return (
     <header>
       <nav className="navbar">
         <p>🍿🎬 Movie Explorer</p>
-        <Link to="/movies">Movies</Link>
-        <Link to="/movies/favorites">Favorites</Link>
+        <Link to="/movies" className="link">
+          Movies
+        </Link>
+        <Link to="/movies/favorites" className="link">
+          Favorites ({favorites.length})
+        </Link>
       </nav>
     </header>
   );
