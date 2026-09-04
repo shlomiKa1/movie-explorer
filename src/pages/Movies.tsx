07588075ef162fe4643573393bf53ecb15140a7c
Shlomi <shlomikaufman91@gmail.com>
2026-09-04 10:14:16 +0300

    feat: add iuse effect for to insure new Render

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,16 +1,40 @@
+import { useEffect, useMemo, useState } from "react";
 import MovieList from "../components/MovieList";
+import SearchBar from "../components/SearchBar";
 import { useFetch } from "../hooks/useFetch";
-
-const URL = "https://api.tvmaze.com/shows?page=1";
+import { mapTvmazeShowToInternal, type TvmazeShowRaw } from "../utils/types";
+import { useMoviesStore } from "../store/MoviesStore";
+import { BASE_URL } from "../utils/types";
 
 const Movies = () => {
-  const { data, loading, error } = useFetch(URL);
+  const { data, loading, error } = useFetch<TvmazeShowRaw[]>(
+    BASE_URL.concat("?page=1"),
+  );
 
-  if (loading) return <p>טוען...</p>;
+  const [search, setSearch] = useState("");
+  const setMovies = useMoviesStore((state) => state.setMovies);
+
+  const movies = useMemo(() => {
+    return data ? data.map(mapTvmazeShowToInternal) : [];
+  }, [data]);
+
+  useEffect(() => {
+    if (movies.length > 0) setMovies(movies);
+  }, [movies]);
+
+  const filteredMovies = useMemo(() => {
+    return movies.filter((movie) =>
+      movie.title.toLowerCase().includes(search.toLowerCase()),
+    );
+  }, [movies, search]);
+
+  if (loading) return <p>Loading data...</p>;
   if (error) return <p>{error}</p>;
+
   return (
-    <div>
-      <MovieList movies={data} />
+    <div className="header">
+      <SearchBar search={search} onSearch={setSearch} />
+      <MovieList movies={filteredMovies} />
     </div>
   );
 };
