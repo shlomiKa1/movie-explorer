8dec7ed7964fdd90bf436e71be552cd084c474ca
Shlomi <shlomikaufman91@gmail.com>
2026-09-04 10:13:03 +0300

    feat:add button for favorite

--- src/components/MovieDetailsPage.tsx
+++ src/components/MovieDetailsPage.tsx
@@ -1,7 +1,19 @@
 import type { MovieProps } from "../utils/types";
+import { useFavoritesStore } from "../store/FavoritesStore";
 import "./MovieDetailsPage.css";
 
 const MovieDetailsPage = ({ movie }: MovieProps) => {
+  const addFavorite = useFavoritesStore((state) => state.addFavorite);
+  const removeFavorite = useFavoritesStore((state) => state.removeFavorite);
+  const isFavorite = useFavoritesStore((state) => state.isFavorite(movie.id));
+
+  function toggleFavorite() {
+    if (isFavorite) {
+      removeFavorite(movie.id);
+    } else {
+      addFavorite(movie);
+    }
+  }
   return (
     <article className="detail-movie">
       <img src={movie.poster ?? "/placeholder.jpg"} alt={movie.title} />
@@ -13,6 +25,7 @@ const MovieDetailsPage = ({ movie }: MovieProps) => {
         <div className="d">
           <p>Release: {movie.releaseYear}</p>
           <p>Rating: {movie.rating}</p>
+          <button onClick={toggleFavorite}>{isFavorite ? "❤️" : "🤍"}</button>
         </div>
       </div>
     </article>
