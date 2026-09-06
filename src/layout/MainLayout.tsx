4009d8e80a8636a0567dc76cdfe4fd392934a03b
Shlomi <shlomikaufman91@gmail.com>
2026-09-06 08:52:57 +0300

    feat: add footer at layout

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import "./style.css";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
@@ -8,6 +9,7 @@ const MainLayout = () => {
       <Header />
       <main>
         <Outlet />
+        <Footer />
       </main>
     </>
   );
